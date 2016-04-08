e51e3a737fabd30cf3c8e2937445e0990f6e6db3
Fredrik Tuomas <fredrik.tuomas@eniro.com>
2016-04-08 16:57:51 +0200

    Wednesday hacking.

--- app/src/main/java/se/jpl/t/fredriktest2/RadiationMonitorView.java
+++ app/src/main/java/se/jpl/t/fredriktest2/RadiationMonitorView.java
@@ -5,14 +5,29 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class RadiationMonitorView extends AppCompatActivity {
+    private static final String TAG = "RadiationMonitorView";
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -85,6 +100,124 @@ public class RadiationMonitorView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://10.0.2.2:1883",
+                        clientId);
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                TextView textView = (TextView) findViewById(R.id.textViewAirPressure);
+                TextView textViewTemperatureOutside = (TextView) findViewById(R.id.textViewTemperatureOutside);
+                TextView textViewPowerUsage = (TextView) findViewById(R.id.textViewPowerUsage);
+
+                JSONObject jObject = new JSONObject(message.toString());
+                if (jObject.getString("id").equalsIgnoreCase("6C2A0000")) {
+                    textView.setText(jObject.getString("svalue1") + " hPa");
+                }
+                if (jObject.getString("id").equalsIgnoreCase("3585")) {
+                    textViewTemperatureOutside.setText(jObject.getString("svalue1") + "°");
+                }
+                if (jObject.getString("id").equalsIgnoreCase("497")) {
+                    Log.d(TAG, message.toString());
+                    textViewPowerUsage.setText(jObject.getString("svalue2") + " kWh");
+                }
+
+                Log.d(TAG, "Sensor " + jObject.getString("name") + " " + jObject.getString("id"));
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                    if (client.isConnected()) {
+                        Log.d(TAG, "We are connected 1.");
+
+                        String topic = "domoticz/out";
+                        int qos = 1;
+                        try {
+                            IMqttToken subToken = client.subscribe(topic, qos);
+                            subToken.setActionCallback(new IMqttActionListener() {
+                                @Override
+                                public void onSuccess(IMqttToken asyncActionToken) {
+                                    Log.d(TAG, "Subscription ok.");
+                                }
+
+                                @Override
+                                public void onFailure(IMqttToken asyncActionToken,
+                                                      Throwable exception) {
+                                    Log.d(TAG, "Subscription failed.");                                }
+                            });
+                        } catch (MqttException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Log.d(TAG, "Connection failed 1.");
+                    }
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        if (client.isConnected()) {
+            Log.d(TAG, "We are connected 2.");
+        } else {
+            Log.d(TAG, "Connection failed 2.");
+        }
+
+
+
+/*
+        while (!client.isConnected()) {
+            Log.d(TAG, "Not connected!");
+        }
+
+        String topic = "domoticz/out";
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+*/
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_radiation_monitor_view);
